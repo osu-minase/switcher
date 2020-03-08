45eae0f18bbf6e0d1e2f19a77025134f55de8988
iamnotramoo <59285081+iamnotramoo@users.noreply.github.com>
2020-03-09 06:09:27 +0800

    I retard i change smtghngvadflm
    i am retard, yes

--- Switcher/Switcher/tr.cs
+++ Switcher/Switcher/tr.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,15 +9,14 @@ namespace Switcher
     class tr
     {
         //NO API, NO,NO
-        public static string misaneIP = "35.228.101.58";
+        public static string minaseIP = "185.255.134.174";
 
-        public static string onminase = "Вы сейчас играете на osu!minase";
+        public static string onminase = "You are now playing on osu!aluri";
 
+        public static string onoff = "You are currently playing on the normal server.";
 
-        public static string onoff = "Вы сейчас играете на оффициальном сервере";
+        public static string switchtominase = "Click once to connect on osu!aluri server.";
 
-        public static string switchtominase = "Перейди на сервер osu!minase";
-
-        public static string switchtooff = "Перейти на официальный сервер";
+        public static string switchtooff = "Click once to disconnect on osu!aluri server.";
     }
 }
